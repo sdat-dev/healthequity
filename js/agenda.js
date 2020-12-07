87d709c52218853c6b483f22a2bc4e56848e6777
sdat-dev <sdat@albany.edu>
2020-12-07 17:15:13 -0500

    auto commit due to data change

--- js/agenda.js
+++ js/agenda.js
@@ -1,6 +1,4 @@
 let requestURL = "data/agenda.json";
-let requestURL = "data/agenda.json";
-let requestURL = "data/agenda.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,4 @@
 let requestURL = "data/questions.json";
-let requestURL = "data/questions.json";
-let requestURL = "data/questions.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
