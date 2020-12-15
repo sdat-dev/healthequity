89b588db6b846f3999ba80067f0002671f21940e
Vijay-Yeruva <Vijayasekhar.yeruva@gmail.com>
2020-12-15 02:05:07 -0500

    updated community partners script

--- js/communitypartners.js
+++ js/communitypartners.js
@@ -75,9 +75,9 @@ $(window).on("load", function () {
                 }
             }
         }
-        // content += '<input id = "search-box" placeholder = "Search Experts...">'+
-        //             '<button id = "search-button" type = "submit"><i class="fa fa-search"></i></button>'+
-        //         '<br><span id = "search-box-results"></span>';
+        content += '<input id = "search-box" placeholder = "Search Experts...">'+
+                   '<button id = "search-button" type = "submit"><i class="fa fa-search"></i></button>'+
+                   '<br><span id = "search-box-results"></span>';
         content +='<div id="experts-content">'+buildPartnersContent(partners)+'</div>';
         addheader(pageheaders);
         let contentElement = document.createElement('div');
@@ -85,10 +85,10 @@ $(window).on("load", function () {
         contentElement.innerHTML = content.trim();
         maincontentContainer.appendChild(contentElement);
         addfooter();
-        // let searchbox = document.getElementById('search-box');
-        // let searchbutton = document.getElementById('search-button');
-        // searchbox.onkeyup = searchfunction;
-        // searchbutton.onclick = searchfunction;
+        let searchbox = document.getElementById('search-box');
+        let searchbutton = document.getElementById('search-button');
+        searchbox.onkeyup = searchfunction;
+        searchbutton.onclick = searchfunction;
     })).catch(errors => {
         console.log(errors);
     })
@@ -133,36 +133,75 @@ let buildPartnersContent = function(partners){
     for(var i=0; i< partners.length; i++){
         if(partners[i].Q12 == "")
             continue;
-        content +='<div class = "search-container expert-info"><img class = "expert-image" src = "assets/images/experts/' + (partners[i]["Q44_Name"] != ''? partners[i].ResponseId+'_'+partners[i]["Q44_Name"]  : 'placeholder.jpg') +'"/> <h2 class = "content-header-no-margin">' +
-        '<a class = "no-link-decoration" href = ' + partners["Q4.3_4"] + '>' + partners[i].Q12 + ' '+ partners[i].Q11 + '</a></h2><h5 class = "content-header-no-margin faculty-title">'+ (partners[i].Q15 != ''? partners[i].Q15 + ',<br>':'') +
-        getInstitution(partners[i]) + '</h5>'+'<p class = "faculty-description"><strong>Email: </strong> <a class = "email-link" href = mailto:' + partners[i].Q13 + 
-        '>'+ partners[i].Q13+ '</a><br>'+ (partners[i].Q14 != ""? '<strong>Phone: </strong>'+ partners[i].Q14 + '<br>': "")+'<strong>Research Interests: </strong>'+ getResearchInterests(partners[i]) + '</p><p>' + 
-        partners[i].Q61 +'</p><br>' + (partners[i].Q61 !="" ? '<strong>Mission and Vision: </strong>' + partners[i].Q61 +'<br>':"") +'</div>';
+        content +='<div class = "search-container expert-info"><img class = "expert-image" src = "assets/images/experts/' + (partners[i]["Q44_Name"] != ''? partners[i].ResponseId+'_'+partners[i]["Q44_Name"]  : 'placeholder.jpg') +'"/>'+
+        '<h2 class = "content-header-no-margin">'+ (partners[i].Q62 != ""? '<a class = "no-link-decoration" href = ' + partners[i].Q62 + '>' + partners[i].Q61 + '</a>': partners[i].Q61) +'</h2>'+
+        '<p class = "address"><b>Address: </b>'+ getAddress(partners[i]) + '</p>'+
+        '<p class = "address"><b>Point Of Contact: </b>'+ getPointOfContact(partners[i]) + '</p>'+
+        '<p><b>Mission and Vision: </b></br>' + formatText(partners[i].Q64) +'</p></div>';
     }
-    console.log("content:"+content);
     return content;
 }
 
-let getInstitution = function(partner){
-    let institution = "";
-    if(partner.Q16 == "University"){
-        institution = partner.Q17;
+let getAddress = function(partner){
+    let address = "";
+    if(partner.Q65 != ""){
+        address += partner.Q65;
     }
-    else if(partner.Q16 == "Community Partner")
+    if(partner.Q66 != "")
     {
-        institution = partner.Q110;
+        address = address == ""? partner.Q66 : (address + ", " +  partner.Q66);
     }
-    else
+    if(partner.Q67 != "")
     {
-        institution = partner["Q17_4_TEXT"];
+        address = address == ""? partner.Q67 : (address + ", " +  partner.Q67);
     }
-    return institution;
+    if(partner.Q68 != "")
+    {
+        address = address == ""? partner.Q68 : (address + ", " +  partner.Q68);
+    }
+    if(partner.Q69 != "")
+    {
+        address = address == ""? partner.Q69 : (address + ", " +  partner.Q69);
+    }
+    return address;
 }
 
-let getResearchInterests = function(partner){
-    let interests = "";
-    interests += (partner["Q51_1"] == ''?"": partner["Q51_1"]+"; ") + (partner["Q51_14"] == ''?"": partner["Q51_14"]+"; ")+ (partner["Q51_15"] == ''?"": partner["Q51_15"]+"; ")+ (partner["Q51_16"] == ''?"": partner["Q51_16"]+"; ") + partner["Q51_17"]; 
-    return interests;
+let getPointOfContact = function(partner){
+    let pointofcontact = "";
+    pointofcontact += partner.Q72 + " " + partner.Q71+ ", "+ partner.Q75 + ' (<a class = "email-link" href = mailto:' + partner.Q73 + 
+    '>'+ partner.Q73+ '</a>'+ (partner.Q74 == ""? '' : ', '+ partner.Q74) +')'; 
+    return pointofcontact;
+}
+
+let formatText = function(text){
+    let result = '';
+    let paras = text.split("\n\n");
+    for(var i=0; i< paras.length; i++){
+        let para = paras[i];
+        if(para.includes("\n") == false && para.search(/d.\t/) == -1)
+        {
+            result += para;
+        }
+        else
+        {
+            let lines = para.split(/\n/);
+            if(lines.length == 1)
+            {
+                result += lines[0]; 
+            }
+            else
+            {
+                for(var j =0; j< lines.length; j++)
+                {
+                    if(lines[j] == '') continue;
+                    result += '<p>'+lines[j]+'</p>'; 
+                }
+            }
+        }        
+    }
+
+
+    return result;
 }
 
 $('.carousel').carousel({pause: false});
