ae918d2d5165e2b55a0ff9199ef4a15c6253f807
Vijay-Yeruva <Vijayasekhar.yeruva@gmail.com>
2020-12-15 01:00:07 -0500

    updated experts page script

--- js/facultyexperts.js
+++ js/facultyexperts.js
@@ -131,47 +131,177 @@ let addheader =  function (headers){
 
 let buildExpertContent = function(experts){
     let content = '';
+    let universityExperts = experts.filter(function(expert){
+        return expert["Q16"] == "University";
+    });
+    let nonuniversityExperts = experts.filter(function(expert){
+        return expert["Q16"] != "University";
+    });
+    let tabattribute = "Q17"
+    let distincttabs = getDistinctUniversities(universityExperts);
+    distincttabs.push("Other Organizations");
+    content = createTabNavigation(distincttabs, tabattribute);
+    let tabContent = [];
+    for(let i = 0; i< distincttabs.length; i++){
+        let tabexperts = universityExperts.filter(function(expert){
+            return (expert.Q17 == distincttabs[i]) || (expert["Q17_4_TEXT"] == distincttabs[i]);
+        });
+        let tabId = "";
+        if(distincttabs[i] != "Other Organizations")
+        {
+            tabId = tabattribute + i.toString();
+            tabContent.push(buildUniversityExperts(tabId, tabexperts));
+        }
+        else
+        {
+            tabId = tabattribute + i.toString();
+            tabContent.push(buildNonUniversityExperts(tabId, nonuniversityExperts));
+        }
+        
+    }
+
+    content += buildTabContent(distincttabs, tabattribute, tabContent);
+    return content;
+}
+
+let getDistinctUniversities = function(experts){
+    let mappedAttributes = experts.map(function(expert){
+        return expert.Q17 != "Other" ? expert.Q17 : expert["Q17_4_TEXT"];
+    });
+    let distinctAttributes = mappedAttributes.filter(function(v, i, a){
+        return a.indexOf(v) === i;
+     });
+
+    return distinctAttributes;
+}
+//Start with level1 accordion and build one by one the levels going down.
+//this is nestted accordion that can go upto 4 levels
+let buildUniversityExperts = function(tabId, tabexperts){
+    let counter = 1; 
+    let contactElem = '';
+    contactElem += '<div id = "' + tabId + '">';
+    let distinctLevel1s = getDistinctAttributes(tabexperts, 'Q18');
+    distinctLevel1s.sort();
+    distinctLevel1s.forEach(function(level1) {
+        let collapseId1 = "collapse" + counter;
+        let headerId1 = "heading" + counter;
+        let childId1 = "child" + counter;
+        counter++;
+        let level2Elem = '';
+        //filter level2s
+        let level2s = tabexperts.filter(function(expert){
+            return expert.Q18 == level1;
+        }); 
+
+        if(level2s.length > 0)
+        {
+            let distinctLevel2s = getDistinctAttributes(level2s, 'Q19');
+            distinctLevel2s.sort();
+            distinctLevel2s.forEach(function(level2){
+                //filter level3 
+                let level3s = level2s.filter(function(expert){
+                    return expert.Q19 == level2;
+                });
+                level3s.sort((a,b) => b.firstName - a.firstName)
+                //for level2s build simple list
+                level2Elem+= buildUniversityExpertElements(level3s);
+            });
+        }  
+        contactElem+= generateAccordionElem(1, collapseId1, headerId1, tabId, childId1, level1, level2Elem);
+    });
+    contactElem += '</div>';
+    //end level1 accordion
+    return contactElem;
+}
+
+let buildUniversityExpertElements = function(experts){
+    let content = '';
     for(var i=0; i< experts.length; i++){
-        if(experts[i].Q12 == "")
+        if(experts[i].Q12 == "") //skip of there is no first name
             continue;
-        content +='<div class = "search-container expert-info"><img class = "expert-image" src = "assets/images/experts/' + (experts[i]["Q44_Name"] != ''? experts[i].ResponseId+'_'+experts[i]["Q44_Name"]  : 'placeholder.jpg') +'"/> <h2 class = "content-header-no-margin">' +
-        '<a class = "no-link-decoration" href = ' + experts[i]["Q4.3_4"] + '>' + experts[i].Q12 + ' '+ experts[i].Q11 + '</a></h2><h5 class = "content-header-no-margin faculty-title">'+ (experts[i].Q15 != ''? experts[i].Q15 + ',<br>':'') +
-        getInstitution(experts[i]) + '</h5>'+ generateLogoContent(experts[i]) +'<p class = "faculty-description"><strong>Email: </strong> <a class = "email-link" href = mailto:' + experts[i].Q13 + 
-        '>'+ experts[i].Q13+ '</a><br>'+ (experts[i].Q14 != ""? '<strong>Phone: </strong>'+ experts[i].Q14 + '<br>': "")+'<strong>Research Interests: </strong>'+ getResearchInterests(experts[i]) + '</p><p>' + 
-        experts[i].Q42 +'</p></div>';
+        let expert = experts[i];
+        content +='<div class = "search-container expert-info">'+
+        '<img class = "expert-image" src = "assets/images/experts/' + (expert["Q44_Name"] != ''? expert.ResponseId+'_'+expert["Q44_Name"]  : 'placeholder.jpg') +'"/>'+
+        '<h2 class = "content-header-no-margin">'+ (expert["Q43_9"] == ""? expert.Q12 + ' '+ expert.Q11 : '<a class = "no-link-decoration" href = ' + expert["Q43_9"] + '>' + expert.Q12 + ' '+ expert.Q11 + '</a>') + '</h2>'+
+        '<h5 class = "content-header-no-margin faculty-title">'+ (expert.Q15 != ''? expert.Q15 + ',<br>':'') + (expert.Q18 != ''? expert.Q18 + ', ':'') + expert.Q17 + '</h5>' +
+        generateLogoContent(expert) +'<p class = "faculty-description"><strong>Email: </strong> <a class = "email-link" href = mailto:' + expert.Q13 + 
+        '>'+ expert.Q13+ '</a><br>'+ (expert.Q14 != ""? '<strong>Phone: </strong>'+ expert.Q14 + '<br>': "")+'<strong>Research Interests: </strong>'+ 
+        getResearchInterests(expert) + '</p><p>' + expert.Q42 +'</p>'+ generateProjectsContent([expert["Q51_1"],expert["Q51_14"],expert["Q51_15"],expert["Q51_16"],expert["Q51_17"]])+'</div>';
     }
     return content;
 }
 
-let getInstitution = function(expert){
-    let institution = "";
-    if(expert.Q16 == "University"){
-        institution = expert.Q17;
+let buildNonUniversityExperts = function(tabId, tabexperts){
+    let counter = 1; 
+    let contactElem = '';
+    contactElem += '<div id = "' + tabId + '">';
+    let distinctLevel1s = getDistinctAttributes(tabexperts, 'Q110');
+    distinctLevel1s.sort();
+    distinctLevel1s.forEach(function(level1) {
+        let collapseId1 = "collapse" + counter;
+        let headerId1 = "heading" + counter;
+        let childId1 = "child" + counter;
+        counter++;
+        let level2Elem = '';
+        //filter level2s
+        let level2s = tabexperts.filter(function(expert){
+            return expert.Q110 == level1;
+        }); 
+        if(level2s.length > 0)
+        {
+            let distinctLevel2s = getDistinctAttributes(level2s, 'Q111');
+            distinctLevel2s.sort();
+            distinctLevel2s.forEach(function(level2){
+                //filter level3 
+                let level3s = level2s.filter(function(expert){
+                    return expert.Q111 == level2;
+                });
+                level3s.sort((a,b) => b.firstName - a.firstName)
+                //for level2s build simple list
+                level2Elem+= buildNonUniversityExpertElements(level3s);
+            });
+        } 
+
+        if(level1 == "")
+        {
+            level1 = "Other";
+        }
+
+        contactElem+= generateAccordionElem(1, collapseId1, headerId1, tabId, childId1, level1, level2Elem);
+    });
+    contactElem += '</div>';
+    //end level1 accordion
+    return contactElem;
+}
+
+let buildNonUniversityExpertElements = function(experts){
+    let content = '';
+    for(var i=0; i< experts.length; i++){
+        if(experts[i].Q12 == "") //skip of there is no first name
+            continue;
+        let expert = experts[i];
+        content +='<div class = "search-container expert-info"><img class = "expert-image" src = "assets/images/experts/' + (expert["Q44_Name"] != ''? expert.ResponseId+'_'+expert["Q44_Name"]  : 'placeholder.jpg') +'"/>'+
+        '<h2 class = "content-header-no-margin">'+ (expert["Q43_9"] == ""? expert.Q12 + ' '+ expert.Q11 : '<a class = "no-link-decoration" href = ' + expert["Q43_9"] + '>' + expert.Q12 + ' '+ expert.Q11 + '</a>') + '</h2>'+
+        '<h5 class = "content-header-no-margin faculty-title">'+ (expert.Q15 != ''? expert.Q15 + ',<br>':'') +
+        (expert.Q111 == ''? '' : expert.Q111 + ', ') + expert.Q110 + '</h5>'+ generateLogoContent(expert) +
+        '<p class = "faculty-description"><strong>Email: </strong> <a class = "email-link" href = mailto:' + expert.Q13 + '>'+ expert.Q13+ '</a><br>'+ 
+        (expert.Q14 != ""? '<strong>Phone: </strong>'+ expert.Q14 + '<br>': "")+'<strong>Research Interests: </strong>'+ getResearchInterests(expert) + '</p>'+
+        '<p>' + experts[i].Q42 +'</p>'+generateProjectsContent([expert["Q51_1"],expert["Q51_14"],expert["Q51_15"],expert["Q51_16"],expert["Q51_17"]])+'</div>';
     }
-    else if(expert.Q16 == "Community Partner")
-    {
-        institution = expert.Q110;
-    }
-    else
-    {
-        institution = expert["Q17_4_TEXT"];
-    }
-    return institution;
+    return content;
 }
 
 let generateLogoContent = function(expert){
     let onlineCVContent = (expert["Q43_4"] == '')?'':
     '<a href = "'+ expert["Q43_4"] +'"><img src = "assets/images/cv.png"></a>'; 
-    let researchGateContent = (expert["Q43_9"]== '')?'':
-    '<a href = "'+ expert["Q43_9"] +'"><img src = "assets/images/research-gate-logo.png"></a>'; 
-    let googleScholarContent = (expert["Q43_10"] == '')?'':
-    '<a href = "'+ expert["Q43_10"] +'"><img src = "assets/images/google-scholar-logo.png"></a>'; 
-    let otherContent = (expert["Q43_11"] == '')?'':
-    '<a href = "'+ expert["Q43_11"] +'"><img src = "assets/images/link.png"></a>'; 
-    let otherContent1 = (expert["Q43_12"] == '')?'':
+    let researchGateContent = (expert["Q43_10"]== '')?'':
+    '<a href = "'+ expert["Q43_10"] +'"><img src = "assets/images/research-gate-logo.png"></a>'; 
+    let googleScholarContent = (expert["Q43_11"] == '')?'':
+    '<a href = "'+ expert["Q43_11"] +'"><img src = "assets/images/google-scholar-logo.png"></a>'; 
+    let otherContent = (expert["Q43_12"] == '')?'':
     '<a href = "'+ expert["Q43_12"] +'"><img src = "assets/images/link.png"></a>'; 
     let linkContainer = '<div class = "display-flex icon-container">'+
-    onlineCVContent + researchGateContent + googleScholarContent + otherContent + otherContent1 + '</div>';
+    onlineCVContent + researchGateContent + googleScholarContent + otherContent + '</div>';
     return linkContainer;
 }
 
@@ -180,6 +310,23 @@ let getResearchInterests = function(expert){
     interests += (expert["Q41_1"] == ''?  "" : expert["Q41_1"] +"; " )+ (expert["Q41_8"] == ''?  "":expert["Q41_8"] +"; ") + (expert["Q41_9"] == ''?  "": expert["Q41_9"]+"; ") + (expert["Q41_10"]== ''?  "":expert["Q41_10"] +"; " )+
     (expert["Q41_11"] == ''?  "":expert["Q41_11"] +"; ") + (expert["Q41_12"]== ''?"":expert["Q41_12"]+"; ") + expert["Q41_13"] ; 
     return interests;
+}
+
+let generateProjectsContent = function(projects){
+    let linkContent = '';
+    let projectcount = 0;
+    for(let i = 0; i < projects.length; i++)
+    {
+      if('' != projects[i])
+      {
+        linkContent = linkContent + '<li>'+ projects[i] + '</li>';
+        projectcount++;
+      }
+    }
+    linkContent = (projectcount > 0)?
+    '<b class = "purple-font">Ongoing Research/Scholarship Related Projects</b><ul class = "sub-list">'
+    + linkContent + '</ul>': '';
+    return linkContent;
 }
 
 $('.carousel').carousel({pause: false});
