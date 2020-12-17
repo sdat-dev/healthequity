45468530082c45466a9c71d72890d7465db36800
sdat-dev <sdat@albany.edu>
2020-12-16 19:08:20 -0500

    Fixed header menu image

--- js/topmenu.js
+++ js/topmenu.js
@@ -6,13 +6,13 @@ let addTopMenu = function(){
                             '<a href="https://www.albany.edu/">'+
                                 '<img class="ualbany-logo" src="assets/images/logo.png" />'+
                             '</a>'+
-                            '<div class="topnav-right" style="margin-left:1000px; display:inline; float: right;"">' +
+                            '<div class="topnav-right" style="display:inline; float: right;"">' +
                                 '<a href="https://www.albany.edu/myualbany">MYUALBANY</a>'+
                                 '<a href="https://www.albany.edu/apply-now">APPLY</a>'+
                                 '<a href="https://www.alumni.albany.edu/s/1642/18-giving/landing.aspx?sid=1642&gid=2&pgid=2040&appealcode=uahome">'+
                                 '   GIVE</a>'+ 
                             '</div>'+
-                            '<div class="searchTop"  style="float: right;">' +
+                            '<div class="searchTop"  style="float: right; display:flex;">' +
                                 '<form class="searchbox"  > '+
                                   '<button type="button" style="padding-left:25px;padding-right:15px; float:right; padding-top:20px;" class="btn1" id="search-toggle">'+
                                         '<span class="fa fa-search" style="line-height: 50px; font-size: 20px;"></i>'+
@@ -214,7 +214,7 @@ $(document).ready(function () {
 
           searchBox.removeClass('searchbox-open');
 
-          $(".topnav-right").animate({ "margin-left":  "1000px" },"fast");    
+          $(".topnav-right").animate({ "margin-left":  "730px" },"fast");    
 
   
           
