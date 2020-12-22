c9cdf0226dc1495586251d89e7cf2726fc239f13
sdat-dev <sdat@albany.edu>
2020-12-22 09:29:13 -0500

    updated webcontent based on excel update

--- js/speakers.js
+++ js/speakers.js
@@ -1,4 +1,4 @@
-let requestURL = "data/speaker.json";
+let requestURL = "data/speakers.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
--- js/zoomlinks.js
+++ js/zoomlinks.js
@@ -1,4 +1,4 @@
-let requestURL = "data/zoomlink.json";
+let requestURL = "data/zoomlinks.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
