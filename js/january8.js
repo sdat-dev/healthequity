d5c71b09a9272e04c5a1fe68851fe24b4f11dc01
Vijay-Yeruva <Vijayasekhar.yeruva@gmail.com>
2021-01-03 19:48:54 -0500

    updated agenda sub pages web content from excel sheet

--- js/january8.js
+++ js/january8.js
@@ -1,21 +1,124 @@
-let requestURL = "data/january8.json";
-let request = new XMLHttpRequest();
-//getting content Element to append grants information
-let maincontentContainer = document.getElementsByClassName('main-content')[0];
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function(){
-    let content = '';
-    const sessionsjson = request.response;
-    //condition for checking if browser is Internet Explorer
-    let sessions =  ((false || !!document.documentMode))? JSON.parse(sessionsjson): sessionsjson;
-    content = buildSessionContent(sessions);
-    let contentElement = document.createElement('div');
-    contentElement.classList.add('content');
-    contentElement.innerHTML = content.trim();
-    maincontentContainer.appendChild(contentElement);
-    addfooter();
+window.onload = function () {
+    let requestURL = "data/january8.json"; 
+    let datarequestURL = "data/sessiondata.json"; 
+    let request =  axios.get(requestURL);
+    let datarequest =  axios.get(datarequestURL);
+    let maincontentContainer = document.getElementsByClassName('main-content')[0];
+    axios.all([request, datarequest]).then(axios.spread((...responses) => {
+        let webelements =  responses[0].data;
+        let sessiondata  = responses[1].data;
+
+        let content = '';
+        let logostart = true;
+        let pageheaders = [];
+        for(let i = 0; i < webelements.length; i++)
+        {
+            let element = webelements[i]; 
+            let type = element.type.toLowerCase(); 
+            if(type == 'ph')
+            {
+                pageheaders.push(element);
+            }
+            else if(type == 'ch')
+            {
+                let header = document.getElementsByClassName("content-header")[0];
+                header.innerHTML = element.content.toUpperCase();
+            }
+            else if(type == 'p')
+            {
+                content += '<p>' + element.content + '</p>';
+            }
+            else if(type == 'img')
+            {
+                content += '<img src="assets/images/'+ element.content + '" alt="" style="width: 100%;">';
+            }
+            else if(type == 'iframe')
+            {
+                content += '<iframe '+ element.content +'></iframe>';
+            }
+            else if(type == 'ul')
+            { 
+                content += '<ul class="sub-list ' + element.content +'">';
+            }
+            else if(type == 'li')
+            {
+                content += '<li>'+ element.content +'</li>';
+            }
+            else if(type == '/ul')
+            {
+                content += '</ul>';
+            }
+            else if(type == 'a' && !element.hasOwnProperty("logo"))
+            {
+                content +='<a href = "'+ element.source +'">'+ element.content + '</a>';
+            }
+            else if(type == 'a' && element.logo != '')
+            {
+                if(logostart == true)
+                {
+                    content +='<div class = "display-flex">';
+                    logostart = false;
+                }
+                content +='<div class = "col-xl-4 col-lg-6 col-md-12">'+
+                            '<a target = "_blank" href = "'+ element.source +'">'+
+                                '<div class = "home-logo-container">' +
+                                    '<img class = "home-logo" src = "assets/images/' + element.logo+ '">'+
+                                    '<p>'+ element.content+'</p>' +
+                                '</div>'+
+                            '</a>'+
+                        '</div>';
+                if(i+1 ==  webelements.length){
+                    content += '</div>';
+                }
+            }
+        }
+        sessions = sessiondata.filter(function(item){
+            return item.Day == "1/8/2021";
+        });
+        content += buildSessionContent(sessions);
+        addheader(pageheaders);
+        let contentElement = document.createElement('div');
+        contentElement.classList.add('content');
+        contentElement.innerHTML = content.trim();
+        maincontentContainer.appendChild(contentElement);
+        addfooter();
+    })).catch(errors => {
+        console.log(errors);
+    })
+}
+
+let addheader =  function (headers){
+    let header = document.getElementById("page-header");
+    let content ="";
+    let image = "";
+    let header1 = "";
+    let header2 = "";
+
+    content += '<div class="carousel slide carousel-fade pointer-event" data-ride="carousel">'+
+                    '<div class="carousel-inner">';
+    for(var i =0 ; i < headers.length; i++)
+    {
+        image = typeof headers[i].logo != 'undefined' && headers[i].logo != ''? headers[i].logo : image;
+        header1 =  typeof headers[i].content != 'undefined' && headers[i].content != ''? headers[i].content : header1;
+        header2 =  typeof headers[i].subcontent != 'undefined' && headers[i].subcontent != ''? headers[i].subcontent : header2;
+        let source = 'assets/images/' + (typeof headers[i].source != 'undefined' && headers[i].source != ''? headers[i].source+'/' : '');
+        if(i == 0)
+        {
+            content += '<div class="carousel-item active">';
+        }
+        else
+        {
+            content += '<div class="carousel-item">';
+        }
+        content +=  '<img src="'+ source + image +'" class="d-block w-100" alt="...">'+
+                    '<div id = "landing-page-text-wrapper">'+
+                        '<h1>'+ header1 +'</h1>' + 
+                        '<p>' + header2 + '</p>' +      
+                    '</div>'+
+                '</div>';
+    }
+    content +=  '</div></div>';
+    header.innerHTML = content;
 }
 
 function getTime(decimaltime) {
@@ -62,7 +165,7 @@ let buildSessionContent =  function (sessions){
             for(var j = 0; j < panelists.length; j++){
                 let member = panelists[j];
                 conent += '<div class= "col-lg-3 col-md-3 col-sm-3" id="'+ (member.FirstName.replace(/ /g, '')) +'">'+
-                '   <p class="text-center"><a target="_blank" href="'+ ((member.FirstName == ""|| member.LastName == "")? '#':'agenda/' + member.FirstName.toLowerCase() + member.LastName.toLowerCase()+ '.html') +'">'+
+                '   <p class="text-center"><a href="'+ ((member.FirstName == ""|| member.LastName == "")? '#':'agenda/' + member.FirstName.toLowerCase() + member.LastName.toLowerCase()+ '.html') +'">'+
                 '   <img class="img-fluid mx-auto d-block panelist-img img-thumbnail" src="assets/images/Panelists/' + member.Photo + '" alt="panelist photo"></a></p>'  +
                 '   <p class="panelist-info dont-break-out"><span class="name">' +  member.FirstName + ' ' + member.LastName + '</span> ' + (member.DegreeCredential == ''? ",": ', ' + member.DegreeCredential+ ',') +
                 (member.JobTitle == ""? "" : '<br><span class="jobtitle">' + member.JobTitle + '</span>,')+
@@ -79,7 +182,7 @@ let buildSessionContent =  function (sessions){
             for(var j = 0; j < moderators.length; j++){
                 let member = moderators[j];
                 conent += '<div class= "col-lg-3 col-md-3 col-sm-3" id="'+ (member.FirstName.replace(/ /g, '')) +'">'+
-                '   <p class="text-center"><a target="_blank" href="'+ ((member.FirstName == ""|| member.LastName == "")? '#':'agenda/' + member.FirstName.toLowerCase() + member.LastName.toLowerCase()+ '.html') +'">'+
+                '   <p class="text-center"><a href="'+ ((member.FirstName == ""|| member.LastName == "")? '#':'agenda/' + member.FirstName.toLowerCase() + member.LastName.toLowerCase()+ '.html') +'">'+
                 '   <img class="img-fluid mx-auto d-block panelist-img img-thumbnail" src="assets/images/Panelists/' + member.Photo + '" alt="member photo"></a></p>'  +
                 '   <p class="panelist-info dont-break-out"><span class="name">' +  member.FirstName + ' ' + member.LastName + (member.DegreeCredential == ''? "": ', ' + member.DegreeCredential + ',') + '</span>'+
                 (member.JobTitle == ""? "" : '<br><span class="jobtitle">' + member.JobTitle + '</span>,')+
