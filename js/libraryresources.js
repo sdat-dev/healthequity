19f084225ac0bc8fd828672085afdf161ae45768
sdat-dev <sdat@albany.edu>
2021-01-28 18:49:05 -0500

    Fixed Funding Opportunity page

--- js/libraryresources.js
+++ js/libraryresources.js
@@ -1,4 +1,4 @@
-let requestURL = "data/proposalpreparation.json";
+let requestURL = "data/libraryresources.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
