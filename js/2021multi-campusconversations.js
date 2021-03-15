09f5735aae2f9db2599b192a61495c187c0607c6
sdat-dev <sdat@albany.edu>
2021-03-15 08:33:12 -0400

    auto commit due to data change

--- js/2021multi-campusconversations.js
+++ js/2021multi-campusconversations.js
@@ -1,4 +1,4 @@
-let requestURL = "data/videoresources.json";
+let requestURL = "data/2021multi-campusconversations.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
