6d8eec54bc8a902e3d26f604576c99cdf964d944
sdat-dev <sdat@albany.edu>
2021-03-16 23:13:13 -0400

    MAJOR UPDATE: Updated proposalprep for 2 xl

--- js/proposalpreparation.js
+++ js/proposalpreparation.js
@@ -1,3 +1,84 @@
+window.onload = function () {
+    let requestURL = "data/proposalpreparation.json";
+    let request = axios.get(requestURL);
+    axios.all([request]).then(axios.spread((...responses) => {
+        let proposalcontent =  responses[0].data;
+        console.log(proposalcontent);
+        let webelements = proposalcontent;
+        let content = '';
+        let logostart = true;
+        let pageheaders = [];
+        for(let i = 0; i < webelements.length; i++)
+        {
+            let element = webelements[i]; 
+            let type = element.type.toLowerCase(); 
+            if(type == 'ph')
+            {
+                pageheaders.push(element);
+            }
+            else if(type == 'ch')
+            {
+                let header = document.getElementsByClassName("content-header")[0];
+                header.innerHTML = element.content.toUpperCase();
+            }
+            else if(type == 'p')
+            {
+                content += '<p>' + element.content + '</p>';
+            }
+            else if(type == 'img')
+            {
+                content += '<img src="assets/images/'+ element.content + '" alt="" style="width: 100%;">';
+            }
+            else if(type == 'iframe')
+            {
+                content += '<iframe '+ element.content +'></iframe>';
+            }
+            else if(type == 'ul')
+            { 
+                content += '<ul class="sub-list ' + element.content +'">';
+            }
+            else if(type == 'li')
+            {
+                content += '<li>'+ element.content +'</li>';
+            }
+            else if(type == '/ul')
+            {
+                content += '</ul>';
+            }
+            else if(type == 'a' && !element.hasOwnProperty("logo"))
+            {
+                content +='<a href = "'+ element.source +'">'+ element.content + '</a>';
+            }
+            else if(type == 'a' && element.logo != '')
+            {
+                if(logostart == true)
+                {
+                    content +='<div class = "display-flex">';
+                    logostart = false;
+                }
+                content +='<div class = "col-xl-4 col-lg-6 col-md-12">'+
+                            '<a target = "_blank" href = "'+ element.source +'">'+
+                                '<div class = "home-logo-container">' +
+                                    '<img class = "home-logo" src = "assets/images/' + element.logo+ '">'+
+                                    '<p>'+ element.content+'</p>' +
+                                '</div>'+
+                            '</a>'+
+                        '</div>';
+                if(i+1 ==  webelements.length){
+                    content += '</div>';
+                }
+            }
+        }
+        addheader(pageheaders);
+        let contentElement = document.createElement('div');
+        contentElement.classList.add('content');
+        contentElement.innerHTML = content.trim();
+        maincontentContainer.appendChild(contentElement);
+
+    })).catch(errors => {
+        console.log(errors);
+    })
+}
 let datarequestURL = "data/proposalpreparationdata.json";
 let datarequest = new XMLHttpRequest();
 //getting content Element to append grants information
@@ -121,7 +202,7 @@ let buildAgencyTabContent = function (distinctAgencies, proposalGuidance, header
             }
             else {
                 accordionElemContent += generateAccordionElem(1, collapseId1, headerId1, agencyId, childId1, header, linkcontent);
-                if (collapseId1 == "collapse1" && agencyId=="agency1") {
+                if (collapseId1 == "collapse1" && agencyId == "agency1") {
                 }
                 accordionCounter++;
             }
