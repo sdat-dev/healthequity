6f0d110eb6312544546c33d179a1de538219fbe2
Sahil Ghauri <sghauri@albany.edu>
2022-01-20 13:51:33 -0500

    changed local references to resources

--- js/callforextendedabstracts.js
+++ js/callforextendedabstracts.js
@@ -1,4 +1,4 @@
-let requestURL = "data/callforextendedabstracts.json";
+let requestURL = "https://sdat-dev.github.io/resources/healthequity/data/callforextendedabstracts.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
